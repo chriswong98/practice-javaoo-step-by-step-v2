57e2489befa363998a9eaa4cfd8c78e524cce686
WONGCH9 <wongch9@oocl.com>
2022-11-30 19:52:14 +0800

    feat: added class Student

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,4 +1,23 @@
 package ooss;
 
-public class Student {
+public class Student extends Person{
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Student (int id, String name, int age){
+        super(id,name,age);
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old. I am a student.";
+    }
+
+
+
 }
