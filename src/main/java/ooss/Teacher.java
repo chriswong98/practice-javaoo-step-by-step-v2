4f7d6f62c025e6e55b3aead31c13573d26861d3e
WONGCH9 <wongch9@oocl.com>
2022-11-30 19:54:08 +0800

    feat: added class Teacher

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,4 +1,25 @@
 package ooss;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Teacher (int id, String name, int age){
+        super(id,name,age);
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old. I am a teacher.";
+    }
+
+
+
+
+
 }
