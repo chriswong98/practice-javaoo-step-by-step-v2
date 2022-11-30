1d9bdd71c4ae107ca2010dff2e8bf778c0464af2
WONGCH9 <wongch9@oocl.com>
2022-11-30 20:08:25 +0800

    feat: added Klass

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,4 +1,32 @@
 package ooss;
 
 public class Klass {
+
+    private int number;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Klass)) return false;
+
+        Klass klass = (Klass) o;
+
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return number;
+    }
+
+    public int getClassNumber() {
+        return number;
+    }
+
+
 }
--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -5,7 +5,7 @@ public class Student extends Person{
     private int id;
     private String name;
     private int age;
-
+    private Klass klass;
     public Student (int id, String name, int age){
         super(id,name,age);
         this.id = id;
@@ -15,9 +15,21 @@ public class Student extends Person{
 
     @Override
     public String introduce() {
-        return "My name is " + name + ". I am " + age + " years old. I am a student.";
+        if (klass != null) {
+            return "My name is " + name + ". I am " + age + " years old. I am a student. I am in class " + klass.getClassNumber() + ".";
+        } else {
+            return "My name is " + name + ". I am " + age + " years old. I am a student.";
+        }
     }
 
 
+    public boolean isIn(Klass klass) {
+        return this.klass==klass;
+    }
+
+
+    public void join(Klass klass) {
+        this.klass=klass;
+    }
 
 }
